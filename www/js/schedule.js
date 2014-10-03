7319afa20f515a01213a4c3aae0c1afcdb57f51f
Ben Hill <ben@neocom.co.nz>
2014-10-03 15:27:39 +1300

    f

--- www/js/schedule.js
+++ www/js/schedule.js
@@ -270,4 +270,23 @@ function getUrlVars() {
     return vars;
 }
 
+function addreminder(){
+
+    var startDate = new Date(2014,10,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
+    var endDate = new Date(2014,10,15,19,30,0,0,0);
+    var title = "My nice event";
+    var location = "Home";
+    var notes = "Some notes about this event.";
+    var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+    var error = function(message) { alert("Error: " + message); };
+
+
+    window.plugins.calendar.createEvent(title,location,notes,startDate,endDate,success,error);
+
+
+}
+
+
+
+
 
