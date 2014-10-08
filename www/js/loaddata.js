2e7cde51aff99b21499cc502b280e4d2ca6bfecf
Ben Hill <ben@neocom.co.nz>
2014-10-09 12:13:59 +1300

    1

--- www/js/loaddata.js
+++ www/js/loaddata.js
@@ -279,7 +279,7 @@ function onclickresync(tx, results) {
 
 
 function successHandler (result) {
-    alert('result = ' + result);
+ //   alert('result = ' + result);
 }
 
 function errorHandler (error) {
@@ -330,14 +330,14 @@ function pushnotifiy() {
 }
 
 function onNotification(e) {
-    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+ //   $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
 
     switch( e.event )
     {
         case 'registered':
             if ( e.regid.length > 0 )
             {
-                $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+              //  $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
                 // Your GCM push server needs to know the regID before it can push to this device
                 // here is where you might want to send it the regID for later use.
                 console.log("regID = " + e.regid);
@@ -347,11 +347,11 @@ function onNotification(e) {
                 $('#busy').show();
                 var strur = 'http://centralfootball.neosportz.com/registerdevice.aspx?deviceID=' + deviceIDfunc + '&devicemodel=' + devicemodelfunc + '&deviceCordova=' + deviceCordovafunc + '&devicePlatform=' + devicePlatformfunc + '&deviceVersion=' + deviceVersionfunc + '&regid=' + e.regid;
                 xmlHttpt.open("GET",strur ,false);
-                alert(strur);
+             //   alert(strur);
                 xmlHttpt.send();
-                var json = xmlHttp.responseText;
 
-                alert(json);
+
+             //   alert(json);
             }
             break;
 
@@ -360,7 +360,7 @@ function onNotification(e) {
             // you might want to play a sound to get the user's attention, throw up a dialog, etc.
             if ( e.foreground )
             {
-                $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+             //   $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
 
                 // on Android soundname is outside the payload.
                 // On Amazon FireOS all custom attributes are contained within payload
@@ -373,27 +373,27 @@ function onNotification(e) {
             {  // otherwise we were launched because the user touched a notification in the notification tray.
                 if ( e.coldstart )
                 {
-                    $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+             //       $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
                 }
                 else
                 {
-                    $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+              //      $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
                 }
             }
 
-            $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+        //    $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
             //Only works for GCM
-            $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+        //    $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
             //Only works on Amazon Fire OS
-            $status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+          //  $status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
             break;
 
         case 'error':
-            $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+          //  $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
             break;
 
         default:
-            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+         //   $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
             break;
     }
 }
