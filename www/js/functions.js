4a57e9811a82c782cb34091fbfd88d56ef345e51
Ben Hill <ben@neocom.co.nz>
2014-10-15 10:11:28 +1300

    1

--- www/js/functions.js
+++ www/js/functions.js
@@ -302,11 +302,13 @@ function syncmaintables(obj){
         });
     });
 
+    db.transaction(function (tx) {
+        tx.executeSql('Delete from MobileStandings');
+        console.log('Delete MobileStandings');
+    });
+
+
     $.each(obj.Standings, function (idx, obj) {
-        db.transaction(function (tx) {
-            tx.executeSql('Delete from MobileStandings where _id =' + obj._id);
-            console.log('Delete MobileStandings');
-        });
 
         db.transaction(function (tx) {
             tx.executeSql('INSERT INTO MobileStandings(_id,Games,Won,Drawn,Lost,ForScore,AgainstScore,Difference,ClubID,Name,TournamentID,FlagPoints,UpdateDateUTC ,TournamentName,DeletedateUTC ) VALUES (' + obj._id + ',' + obj.Games + ',' + obj.Won + ',' + obj.Drawn + ',' + obj.Lost + ',' + obj.ForScore + ',' + obj.AgainstScore + ',' + obj.Difference + ',' + obj.ClubID + ',"' + obj.Name + '",' + obj.TournamentID + ',' + obj.FlagPoints + ',"' + obj.UpdateDateUTC + '","' + obj.TournamentName + '","' + obj.DeletedateUTC + '")');
