82d19033752798805b6d69b4b86664d59e3c032d
Ben Hill <ben@neocom.co.nz>
2014-10-20 11:53:06 +1300

    1

--- www/src/android/com/plugin/gcm/GCMIntentService.java
+++ www/src/android/com/plugin/gcm/GCMIntentService.java
@@ -108,7 +108,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 				.setContentIntent(contentIntent)
 				.setAutoCancel(true);
 
-		String clubID = extras.getString("clubID");
+		String ID = extras.getString("ID");
 		
 				
 				
@@ -136,7 +136,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 			Log.e(TAG, "Number format exception - Error parsing Notification ID" + e.getMessage());
 		}
 		
-		if(clubID == "0"){
+		if(ID.equals("0")){
 		
 			mNotificationManager.notify((String) appName, notId, mBuilder.build());
 		}
