316fa39af8fc4f7b86b78f6aa3c079856231ee36
Ben Hill <ben@neocom.co.nz>
2014-10-22 15:39:21 +1300

    65

--- www/js/functions.js
+++ www/js/functions.js
@@ -241,14 +241,18 @@ var totalnew =0;
     totalnew =  checkintvalue(obj.App_Results.length) + checkintvalue(obj.clubs.length) + checkintvalue(obj.App_Schedule.length)+ checkintvalue(obj.clubsimages.length)+ checkintvalue(obj.vwApp_Teams.length)+ checkintvalue(obj.vwApp_News_v_2.length)+ checkintvalue(obj.App_Players.length)+ checkintvalue(obj.App_Players_Images.length)+ checkintvalue(obj.ScoringTable.length)+ checkintvalue(obj.Standings.length)+ checkintvalue(obj.sponsorsclub.length)+ checkintvalue(obj.screenimage.length);
 
 
+   // alert(percent);
+
+
+   // document.getElementById("ft-prog").value = percenttotal;
 
 
     var datenow1 = new Date();
     var timenow = datenow1.getTime();
 
-    window.plugins.toast.showShortCenter('Updating Tables!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});
+  //  window.plugins.toast.showShortCenter('Updating Tables!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});
 
-
+  //  navigator.splashscreen.show();
 
     $.each(obj.App_Results, function (idx, obj) {
         percenttotal =+ percent;
@@ -420,12 +424,13 @@ var totalnew =0;
 
     db.transaction(CleanDB, errorCBfunc, successCBfunc);
 
-    setTimeout(function(){
-        window.plugins.toast.showLongCenter('Your App is Updated!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});
-        $('#busy').hide();
-    }, 4000);
+  //  setTimeout(function(){
+ //       window.plugins.toast.showLongCenter('Your App is Updated!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});
+  //      $('#busy').hide();
+  //  }, 2000);
 
-
+    window.plugins.toast.showLongCenter('Your App is Updated!', function(a){console.log('toast success: ' + a)}, function(b){alert('toast error: ' + b)});
+    $('#busy').hide();
 
 
 
