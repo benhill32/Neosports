b68d6287d889b8f48d0dc5975d2feffe2edf2c5e
Ben Hill <ben@neocom.co.nz>
2014-10-31 11:05:00 +1300

    1

--- www/js/functions.js
+++ www/js/functions.js
@@ -179,7 +179,7 @@ function passnewfeedtoserver(testvar){
 
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
-             alert(http.responseText);
+           //  alert(http.responseText);
         }
     }
 
