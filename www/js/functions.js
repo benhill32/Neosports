df789de7472eb4af9f76301036ec14e60256ba59
Ben Hill <ben@neocom.co.nz>
2014-11-03 10:00:18 +1300

    1

--- www/js/functions.js
+++ www/js/functions.js
@@ -196,7 +196,7 @@ function passcancelgametoserver(testvar){
     var url = "http://centralfootball.neosportz.com/apploadcancelgame.aspx";
     var params = "?" + testvar;
     http.open("POST", url + params, true);
-
+    alert(url + params);
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
             //  alert(http.responseText);
