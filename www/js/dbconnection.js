9982f5ac9b034bd219af6d330512a8a967e01794
Ben Hill <ben@neocom.co.nz>
2014-11-17 11:43:07 +1300

    1

--- www/js/dbconnection.js
+++ www/js/dbconnection.js
@@ -1,9 +1,19 @@
 var db;
+var deviceplatformdb;
 document.addEventListener("deviceready", onDeviceReadydbconn, false);
 
 
 function onDeviceReadydbconn() {
+    deviceplatformdb = device.platform;
 
-    db = window.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
+    if(deviceplatformdb == "iOS"){
+        alert(deviceplatformdb);
+        db = window.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
+    }else if(deviceplatformdb == "Android"){
+        alert(deviceplatformdb);
+        db = window.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
+    }
+
+
 
 }
