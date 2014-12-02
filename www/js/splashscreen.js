f67024db1867eaea03eddea478240af25f5729dd
Ben Hill <ben@neocom.co.nz>
2014-12-03 12:39:51 +1300

    1

---
+++ www/js/splashscreen.js
@@ -0,0 +1,46 @@
+var db;
+var deviceIDfunc;
+
+
+document.addEventListener("deviceready", onDeviceReadysplashscreen, false);
+
+function onDeviceReadysplashscreen() {
+    deviceIDfunc = device.uuid;
+
+    db.transaction(getbackground, errorCBfunc, createtables);
+}
+
+
+
+
+
+
+
+function getbackground(tx) {
+   // alert($('#mainbackground').css('opacity'));
+
+    var sql = "select Base64 from Mobilescreenimage order by UpdateDateUTC desc LIMIT 1";
+     //alert(sql);
+    tx.executeSql(sql, [], getbackground_success);
+}
+
+function getbackground_success(tx, results) {
+
+    var len = results.rows.length;
+    //alert(len);
+    if(len != 0) {
+        var menu = results.rows.item(0);
+
+        var base64 = menu.Base64;
+        var width = $( window ).width() + "px";
+
+        $('#screensplashimg').attr('src',"data:image/png;base64," + menu.Base64);
+        $('#screensplashimg').css('width',width);
+    }
+}
+
+
+
+
+
+
