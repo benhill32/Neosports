099121f3fe54440048347d3c3bbb2e15d0e9dd71
Ben Hill <ben@neocom.co.nz>
2014-12-02 14:15:09 +1300

    1

--- www/js/loaddata.js
+++ www/js/loaddata.js
@@ -24,7 +24,7 @@ document.addEventListener("deviceready", onDeviceReadyloaddata, false);
 function onDeviceReadyloaddata() {
 
   //  db = window.openDatabase("Neosportz_Football", "1.1", "Neosportz_Football", 200000);
-    console.log("LOCALDB - Database ready");
+    //console.log("LOCALDB - Database ready");
     deviceIDfunc = device.uuid;
     devicePlatformfunc = device.platform;
      getnetworkdetails();
