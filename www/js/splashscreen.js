6ca1e376250c23cc7a20973eb4708ff121b03bb1
Ben Hill <ben@neocom.co.nz>
2014-12-03 13:21:11 +1300

    1

--- www/js/splashscreen.js
+++ www/js/splashscreen.js
@@ -38,7 +38,8 @@ function getbackground_success(tx, results) {
 
         var base64 = menu.Base64;
         var width = $( window ).width() + "px";
-
+alert(width);
+        
         $('#screensplashimg').attr('src',"data:image/png;base64," + menu.Base64);
         $('#screensplashimg').css('width',width);
     }
