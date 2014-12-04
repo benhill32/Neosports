d910e6ae3b6803a3749d13ba5268e8bc0a0ec982
Ben Hill <ben@neocom.co.nz>
2014-12-04 13:21:37 +1300

    1

--- www/js/splashscreen.js
+++ www/js/splashscreen.js
@@ -1,6 +1,6 @@
 var db;
 var orientationstand = "";
-
+var Base64 = "";
 
 document.addEventListener("deviceready", onDeviceReadysplashscreen, false);
 
@@ -24,30 +24,54 @@ function getorientsplash(strorein){
 function getbackground(tx) {
    // alert($('#mainbackground').css('opacity'));
 
-    var sql = "select Base64,URLLINK from Mobilescreenimage order by UpdateDateUTC desc LIMIT 1";
+    var sql = "select Base64 from Mobilescreenimage order by UpdateDateUTC desc LIMIT 1";
      //alert(sql);
     tx.executeSql(sql, [], getbackground_success);
 }
 
 function getbackground_success(tx, results) {
 
+
     var len = results.rows.length;
    //- alert(len);
     if(len != 0) {
         var menu = results.rows.item(0);
-        //alert('<img id="screensplashimg" style="max-height:100%;max-width:100%" onclick="URLredirect(\'' + menu.URL + '\')" src="data:image/png;base64,' + menu.Base64 + '">');
-        //alert(menu.URLLINK);
-        var base64 = menu.Base64;
-        var width = $( window ).width() + "px";
+        var Base64 = menu.Base64;
+
+
+        db.transaction(getbackground, errorCBfunc, successCBfunc);
+
+
+
+    }else{
+
+        window.location.href='../index.html';
+    }
+}
+
+
+function getbackground2(tx) {
+    var sql = "select URLLINK from Mobilescreenimage order by UpdateDateUTC desc LIMIT 1";
+    tx.executeSql(sql, [], getbackground_success2,getbackground2_error);
+}
+
+function getbackground_success2(tx, results) {
+
+    var len = results.rows.length;
+    //- alert(len);
+    if(len != 0) {
+        var menu = results.rows.item(0);
         $('#splashscreen').empty();
-
-
-        $('#splashscreen').append('<img id="screensplashimg" style="max-height:100%;max-width:100%" onclick="URLredirect(\'' + menu.URLLINK + '\')" src="data:image/png;base64,' + menu.Base64 + '">');
+        $('#splashscreen').append('<img id="screensplashimg" style="max-height:100%;max-width:100%" onclick="URLredirect(\'' + menu.URLLINK + '\')" src="data:image/png;base64,' + Base64 + '">');
 
     }
 }
 
 
+function getbackground2_error(err) {
+    $('#splashscreen').empty();
+    $('#splashscreen').append('<img id="screensplashimg" style="max-height:100%;max-width:100%" src="data:image/png;base64,' + Base64 + '">');
+}
 
 
 
